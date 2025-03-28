100b181a6f55c212bd9f5a3af77338f1e3e07ad5
William Brito <williambrito74@hotmail.com>
2025-03-28 17:09:56 -0400

    Inject ApiClientService into AppComponent for API interactions

--- countries.ng/src/app/app.component.ts
+++ countries.ng/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiClientService } from './services/api-client.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'countries.ng';
+  constructor(private apiClient: ApiClientService) {}
 }
