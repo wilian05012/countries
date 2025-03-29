0c1c2e3b6fb7ad4a3fe56aedce44f44f3eaa58c3
William Brito <williambrito74@hotmail.com>
2025-03-28 21:10:18 -0400

    Display world statistics in the app component and implement data retrieval on view initialization

--- countries.ng/src/app/app.component.ts
+++ countries.ng/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ApiClientService } from './services/api-client.service';
+import { CommonModule } from '@angular/common';
+import { WorldStatsService } from './services/world-stats.service';
+import { WorldStats } from './core/world-stats';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  constructor(private apiClient: ApiClientService) {}
+export class AppComponent implements AfterViewInit {
+  worldStats: WorldStats | null = null;
+
+  constructor(private worldStatsService: WorldStatsService) { }  
+  
+  ngAfterViewInit(): void {
+    this.worldStats = this.worldStatsService.getWorldStats();
+  }
 }
